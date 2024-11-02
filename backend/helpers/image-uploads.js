5563307a8df4144a638e5d2fac2e0745b8104b0c
pedrofelipemoreira <pfelope@gmail.com>
2024-11-02 17:17:18 -0300

    iniciando upload de images para perfil

--- backend/helpers/image-uploads.js
+++ backend/helpers/image-uploads.js
@@ -3,3 +3,34 @@ import path from "path";
 
 // Destination store  the  images
 
+const imageStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+
+        let folder = ""
+
+        if(req.baseUrl.includes("uesers")){
+            folder = "users"
+        }
+
+        cb(null, `public/images/${folder}`)
+
+    },
+
+    filename: function(rq, file, cb){
+
+        cb(null, Date.now() + path.extname(file.originalname))
+
+    }
+})
+
+const imageUpload = multer({
+    storage: imageStorage,
+    fileFilter(req, file, cb){
+        if(!file.originalname.match(/\.(png|jpg)$/)){
+            return cb(new Error("por favor, envie apenas jpg ou png!!"))
+        }
+        cb(undefined, true)
+    }
+})
+
+export default imageUpload
